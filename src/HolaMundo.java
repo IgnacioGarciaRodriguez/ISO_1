ac6bff21119e7810c2b431f2676ddabcc84a60b4
ignaciogarciarodriguez <ignaciogarciarodriguez@localhost>
2020-10-02 10:05:35 +0200

    Se ha añadido una nueva clase MessageAgent

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,5 +3,8 @@ public class HolaMundo {
 	public static void main(String args[]) {
 		System.out.println("Hola Mundo");
 		System.out.println("segunda etapa");
+		
+		MessageAgent ma = new MessageAgent("Hola soy tu agente");
+		System.out.print("Agente dice: "+ ma.getMessage());
 	}
 }
