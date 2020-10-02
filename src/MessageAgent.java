e3a172018742d0d0ef7fd6fe40230843f0576013
ignaciogarciarodriguez <ignaciogarciarodriguez@localhost>
2020-10-02 10:05:53 +0200

    Nueva clase MessageAgent

---
+++ src/MessageAgent.java
@@ -0,0 +1,16 @@
+
+public class MessageAgent {
+	
+	private String theMessage ="";
+	
+	public MessageAgent(String m) {
+		
+		this.theMessage = m;
+		
+	}
+	
+	public String getMessage() {
+		return this.theMessage;
+	}
+	
+}
